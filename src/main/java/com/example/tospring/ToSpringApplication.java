cda00f492b5c9dc2b8ca943a1d9552f176263c05
Bigthugboy <rapilomme@gmail.com>
2022-10-31 00:23:06 +0100

    a social media app built with springboot and connect with front-end

---
+++ src/main/java/com/example/tospring/ToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToSpringApplication.class, args);
+    }
+
+}
